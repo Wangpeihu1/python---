b2fb44aa33bd6ef9c7aa172ccbaa0b15d3b9affa
Wangpeihu1 <w18817392281@163.com>
2018-04-21 00:50:49 +0800

    自动生成标签
    使用列表解析代替for循环

--- die_visual.py
+++ die_visual.py
@@ -5,19 +5,23 @@ die_1=Die()
 die_2=Die()
 #掷几次骰子，并将结果存放在列表中
 results=[]
-for roll_num in range(3000):
-	result = die_1.roll()+die_2.roll()
-	results.append(result)
+# for roll_num in range(3000):
+# 	result = die_1.roll()+die_2.roll()
+# 	results.append(result)
+results=[die_1.roll()+die_2.roll() for roll_num in range(3000)]
 #分析结果
-frequencies=[]
 max_result=die_1.numsides+die_2.numsides
-for value in range(2,max_result+1):
-	frequency=results.count(value)
-	frequencies.append(frequency)
+# for value in range(2,max_result+1):
+# 	frequency=results.count(value)
+# 	frequencies.append(frequency)
+frequencies=[results.count(value) for value in range(2,max_result+1)]
+#制作X轴列表循环
+nums=[num for num in range(2,max_result+1)]
+
 #对结果进行可视化
 hist = pygal.Bar()
 hist.title = 'Results of rolling one D6 1000 times.'
-hist.x_labels = ['2','3','4','5','6','7','8','9','10','11','12']
+hist.x_labels = nums
 hist.x_title = 'Result'
 hist.y_title = 'Frequency of Result'
 hist.add('D6+D6',frequencies)
