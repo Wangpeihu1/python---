9dd6a1be7fa178af913efa1843cb57f8b6f75c22
Wangpeihu1 <w18817392281@163.com>
2018-04-20 15:34:51 +0800

    matplotlib test
    随机漫步练习完成

--- random_walk.py
+++ random_walk.py
@@ -7,17 +7,16 @@ class RandomWalk():
 		#所有随机漫步都始于（0,0）
 		self.x_values = [0]
 		self.y_values = [0]
-
+	def get_step(self):
+		g_direction = choice([1,-1])
+		g_distance = choice([0,1,2,3,4])
+		g_step = g_direction*g_distance
+		return g_step
 	def fill_walk(self):
 		#不断漫步，直到列表到达指定长度
 		while len(self.x_values)<self.num_points:
-			x_direction = choice([1,-1])
-			x_distance = choice([0,1,2,3,4])
-			x_step = x_direction*x_distance
-
-			y_direction = choice([1,-1])
-			y_distance = choice([0,1,2,3,4])
-			y_step = y_direction*y_distance
+			x_step=self.get_step()
+			y_step=self.get_step()
 			#拒绝原地踏步
 			if x_step == 0 and y_step == 0:
 				continue
@@ -28,5 +27,8 @@ class RandomWalk():
 			self.x_values.append(next_x)
 			self.y_values.append(next_y)
 
+
+			
+			
 
 
--- rw_visual.py
+++ rw_visual.py
@@ -4,10 +4,18 @@ from random_walk import RandomWalk
 #程序处于活动状态，就不断模拟随机漫步
 while True:
 	#创建一个RW实例，并绘制
-	rw = RandomWalk()
+	rw = RandomWalk(5000)
 	rw.fill_walk()
+	#设置绘图窗口的尺寸
+	plt.figure(figsize=(10,6))
 	point_number=list(range(rw.num_points))
-	plt.scatter(rw.x_values,rw.y_values,c=point_number,cmap=plt.cm.Reds,edgecolor='none',s=15)
+	plt.plot(rw.x_values,rw.y_values,c='blue',linewidth=0.1)
+	#突出起点和终点
+	plt.scatter(0,0,c='green',edgecolor='none',s=100)
+	plt.scatter(rw.x_values[-1],rw.y_values[-1],c='red',edgecolor='none',s=100)
+	#隐藏坐标轴
+	plt.axes().get_xaxis().set_visible(False)
+	plt.axes().get_yaxis().set_visible(False)
 	plt.show()
 
 	keep_running = input('Make another walk ? (y/n): ')
