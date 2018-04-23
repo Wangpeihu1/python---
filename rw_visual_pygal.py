dd7d9a9edacd7a6b6cf446549315338fe50bd9d0
Wangpeihu1 <w18817392281@163.com>
2018-04-23 21:02:05 +0800

    pygal 可视化随机漫步
    今天查看pygal官方手册看到XY挺适合做随机漫步的，就是有些乱

--- rw_visual_pygal.py
+++ rw_visual_pygal.py
@@ -1,5 +1,9 @@
 import pygal
-from random_walk.py import RandomWalk
+from random_walk import RandomWalk
 
-rw = RandomWalk(100)
-rw.fill_work()
+rw = RandomWalk(1000)
+rw.fill_walk()
+rw_chart = pygal.XY(storke=False)
+rw_chart.title = 'RandomWalk'
+rw_chart.add('rw',[(rw.x_values[num],rw.y_values[num]) for num in range(len(rw.x_values))])
+rw_chart.render_to_file('123')
