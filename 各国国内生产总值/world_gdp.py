11bf1bafaee4278a4338a5eec72b532c87b96702
Wangpeihu1 <w18817392281@163.com>
2018-04-24 23:32:44 +0800

    csv+wroldmaps练习
    网上下载CSV资源生成字典配合worldmaps使用

--- 各国国内生产总值/world_gdp.py
+++ 各国国内生产总值/world_gdp.py
@@ -1,22 +1,28 @@
 import pygal
 import pygal.maps.world as pw
 from country_codes import get_country_code
-import json
+import csv
 
 #导入数据
-filename = 'world_gdp.json'
-with open(filename) as f:
-	gdp_data = json.load(f)
-
+filename = 'gdp.csv'
+with open(filename,) as f:
+	gdp_data = csv.reader(f)
+	for i in range(5):
+		header = next(gdp_data)
+		
 #获取每个国家2016年的GDP，做一个字典
-gdps_dict={}
-for gdp_dict in gdp_data:
-	if gdp_dict['Year'] == '2016':
-		country_name = gdp_dict['Country Name']
-		gdp = int(float(gdp_dict['Value']))
-		code = get_country_code(country_name)
-		if code:
-			gdps_dict[code] = gdp
+	gdps_dict={}
+	for gdp_dict in gdp_data:
+		try:
+			country_name = gdp_dict[0]
+			gdp = int(gdp_dict[61])
+			print(int(gdp_dict[61]))
+		except ValueError:
+			print('Error')
+		else:
+			code = get_country_code(country_name)
+			if code:
+					gdps_dict[code] = gdp
 
 wg = pw.World()
 wg.title = '世界各国GDP'
