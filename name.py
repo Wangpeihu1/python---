0d91fb67fba0dabcd3b04a8a4c357a34cb695bcb
Wangpeihu1 <w18817392281@163.com>
2018-05-20 13:52:51 +0800

    Python基础复习
    打印变量
    对变量调用些方法再打印

---
+++ name.py
@@ -0,0 +1,12 @@
+name = 'ada loVElace'
+print(name.title(),name.upper(),name.lower(),)
+
+first_name = 'ada'
+last_name = 'lovelace'
+full_name = first_name+' '+last_name
+print(full_name)
+
+print('Languages:\n\tPython\n\tC\n\tJavascript')
+
+language=' Python 3 '
+print(language+'.'+language.strip()+'.')
