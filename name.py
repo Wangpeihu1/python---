7b26517b1aa331b231213866b56c1dceb66fb3bf
Wangpeihu1 <w18817392281@163.com>
2018-05-20 14:02:58 +0800

    Python基础复习
    对字符串的一些操作

---
+++ name.py
@@ -0,0 +1,19 @@
+name = 'ada loVElace'
+print(name.title(),name.upper(),name.lower(),)
+
+first_name = 'ada'
+last_name = 'lovelace'
+full_name = first_name+' '+last_name
+print(full_name)
+
+print('Languages:\n\tPython\n\tC\n\tJavascript')
+
+language=' Python 3 '
+print(language+'.'+language.strip()+'.')
+
+message = ' “Hello Eric, would you like to learn some Python today?”'
+print(message)
+name_1 = '\tAlbert Einstein\n'
+sentence = '“A person who never made a mistake never tried anything new.”'
+celebrity_motto = name_1.strip()+' once said,'+sentence
+print(celebrity_motto)
